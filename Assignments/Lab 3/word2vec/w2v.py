1c36c54c654cb547531aa8fce9b0aaa3e0a79ab2
Rakin <54189419+RakinAli@users.noreply.github.com>
2024-04-28 17:30:24 +0200

    restart all again

--- Assignments/Lab 3/word2vec/w2v.py
+++ Assignments/Lab 3/word2vec/w2v.py
@@ -5,9 +5,7 @@ import string
 from collections import defaultdict
 import numpy as np
 from sklearn.neighbors import NearestNeighbors
-import random
 
-import re
 from tqdm import tqdm
 
 
@@ -45,21 +43,32 @@ class Word2Vec(object):
         self.__use_corrected = use_corrected
         self.__use_lr_scheduling = use_lr_scheduling
 
+
     def init_params(self, W, w2i, i2w):
-        # __w is the word embeddings, __w2i is a dictionary mapping words to their index in the vocabulary
         self.__W = W
         self.__w2i = w2i
         self.__i2w = i2w
 
+
     @property
     def vocab_size(self):
         return self.__V
+        
 
     def clean_line(self, line):
-        # YOUR CODE HERE - Done
+        """
+        The function takes a line from the text file as a string,
+        removes all the punctuation and digits from it and returns
+        all words in the cleaned line as a list
+        
+        :param      line:  The line
+        :type       line:  str
+        """
+        #
+        # REPLACE WITH YOUR CODE HERE
+        #
+        return []
 
-        # Could not find the python library function to remove
-        return re.sub(r"\d|[^\w\s]", "", line).lower().split()
 
     def text_gen(self):
         """
@@ -79,119 +88,37 @@ class Word2Vec(object):
                 for line in f:
                     yield self.clean_line(line)
 
+
     def get_context(self, sent, i):
         """
         Returns the context of the word `sent[i]` as a list of word indices
-
+        
         :param      sent:  The sentence
         :type       sent:  list
         :param      i:     Index of the focus word in the sentence
         :type       i:     int
         """
+        #
+        # REPLACE WITH YOUR CODE
+        # 
+        return []
 
-        # Extracting left and right window sizes from class attributes
-        left_window_size = self.__lws
-        right_window_size = self.__rws
-
-        # Retrieving the length of the sentence
-        sent_length = len(sent)
-
-        # Constructing a list of context indices within the sentence
-        context_indices_in_sent = list(range(i - left_window_size, i)) + list(
-            range(i + 1, i + right_window_size + 1)
-        )
-
-        # Ensuring that context indices are within the bounds of the sentence
-        context_indices_in_sent = [
-            item for item in context_indices_in_sent if 0 <= item < sent_length
-        ]
-
-        # Extracting context words from the sentence based on context indices
-        context_words = [sent[index] for index in context_indices_in_sent]
-
-        # Getting the respective context indices in the entire corpus
-        context_indices = [self.w2i[word] for word in context_words]
-
-        return context_indices
 
     def skipgram_data(self):
         """
         A function preparing data for a skipgram word2vec model in 3 stages:
         1) Build the maps between words and indexes and vice versa
         2) Calculate the unigram distribution and corrected unigram distribution
-        (the latter according to Mikolov's article)
+           (the latter according to Mikolov's article)
         3) Return a tuple containing two lists:
             a) list of focus words
             b) list of respective context words
         """
+        #
         # REPLACE WITH YOUR CODE
+        # 
+        return [], []
 
-        """
-        A function preparing data for a skipgram word2vec model.
-        """
-        self.w2i = {}
-        self.i2w = {}
-        self.unigram_count = {}
-        self.__V = 0  # Vocabulary size
-        self.__TotalWords = 0  # Total number of words in the corpus
-
-        index = 0
-        for line in self.text_gen():
-            for word in line:
-                self.__TotalWords += 1
-                if word not in self.w2i:
-                    self.w2i[word] = index
-                    self.i2w[index] = word
-                    self.unigram_count[word] = 1
-                    self.__V += 1  # Increment vocabulary size only for new words
-                    index += 1
-                else:
-                    self.unigram_count[word] += 1
-
-        """
-        # Optionally, write the w2i dictionary to a text file
-        with open("w2i.txt", 'w') as f:
-            for key, value in self.w2i.items():
-                f.write('%s:%s\n' % (key, value))
-        """
-
-        # Calculate the unigram distribution
-        self.unigram_distribution = {}
-        total_words = sum(self.unigram_count.values())
-        for word in self.unigram_count:
-            self.unigram_distribution[word] = self.unigram_count[word] / total_words
-
-        # Calculate the corrected unigram distribution
-        """
-        Slide 40 Lecture 6: P_s(w) = P_unigram(w)^0.75 / sum(P_unigram(w)^0.75)
-
-        Basically sum the denom
-        """
-        self.corrected_unigram = {}
-        denominator = 0
-        for word in self.unigram_distribution:
-            denominator += self.unigram_distribution[word] ** 0.75
-        for word in self.unigram_distribution:
-            self.corrected_unigram[word] = self.unigram_distribution[word] ** 0.75 / denominator
-
-        # Step 3: Return two two lists: Focus words and context words
-        focus_words = []
-        context_indices = []
-        for line in self.text_gen():
-            for i, word in enumerate(line):
-                if word not in focus_words:
-                    focus_words.append(word)
-                    context_indices.append(self.get_context(line, i))
-                if word in focus_words:
-                    focus_index = focus_words.index(word)
-                    context_indices[focus_index].extend(self.get_context(line, i))
-
-        # Print w2i as txt
-        with open("w2i.txt", 'w') as f:
-            for key, value in self.w2i.items():
-                f.write('%s:%s\n' % (key, value))
-
-        return focus_words, context_indices
 
     def sigmoid(self, x):
         """
@@ -199,58 +126,24 @@ class Word2Vec(object):
         """
         return 1 / (1 + np.exp(-x))
 
-    import numpy as np
-
-    def create_alias_table(self,probs):
-        n = len(probs)
-        scaled_probs = np.array(probs) * n
-        aliases = np.zeros(n, dtype=np.int32)
-        small = []
-        large = []
-        
-        for i, prob in enumerate(scaled_probs):
-            if prob < 1.0:
-                small.append(i)
-            else:
-                large.append(i)
-        
-        while small and large:
-            small_index = small.pop()
-            large_index = large.pop()
-            
-            aliases[small_index] = large_index
-            scaled_probs[large_index] = (scaled_probs[large_index] - 1) + (scaled_probs[small_index] - 1)
-            
-            if scaled_probs[large_index] < 1.0:
-                small.append(large_index)
-            else:
-                large.append(large_index)
-        
-        return scaled_probs, aliases
-
 
     def negative_sampling(self, number, xb, pos):
         """
-        Sample a `number` of negatives examples avoiding the words in `xb` and `pos` words.
+        Sample a `number` of negatives examples with the words in `xb` and `pos` words being
+        in the taboo list, i.e. those should be replaced if sampled.
+        
+        :param      number:     The number of negative examples to be sampled
+        :type       number:     int
+        :param      xb:         The index of the current focus word
+        :type       xb:         int
+        :param      pos:        The index of the current positive example
+        :type       pos:        int
         """
-        use_corrected = self.__use_corrected
-        unigram = self.corrected_unigram if use_corrected else self.unigram_distribution
+        #
+        # REPLACE WITH YOUR CODE
+        #
+        return []
 
-        negative_samples_indices = []
-        keys = list(unigram.keys())
-        probabilities = np.array(list(unigram.values()))
-        probabilities /= probabilities.sum()  # Normalize probabilities
-
-        while len(negative_samples_indices) < number:
-            candidates = np.random.choice(keys, size=number*2, p=probabilities)
-            for candidate in candidates:
-                candidate_index = self.w2i[candidate]
-                if candidate_index not in negative_samples_indices and candidate_index != xb and candidate_index != pos:
-                    negative_samples_indices.append(candidate_index)
-                if len(negative_samples_indices) == number:
-                    break
-
-        return negative_samples_indices
 
     def train(self):
         """
@@ -261,84 +154,16 @@ class Word2Vec(object):
         print("Dataset contains {} datapoints".format(N))
 
         # REPLACE WITH YOUR RANDOM INITIALIZATION
-        self.__W = np.random.rand(self.__V, self.__H)
-        self.__U = np.random.rand(self.__V, self.__H)
+        self.__W = np.zeros((100, 50))
+        self.__U = np.zeros((100, 50))
 
         for ep in range(self.__epochs):
             for i in tqdm(range(N)):
+                #
+                # YOUR CODE HERE 
+                #
+                pass
 
-                # YOUR CODE HERE
-
-                # Get the focus word and its index
-                focus_word = x[i]
-                focus_word_index = self.w2i[focus_word]
-                # Get the context words and their indices
-                postive_samples_indices = t[i]
-                postive_samples = [self.i2w[index] for index in postive_samples_indices]
-
-                # Generate negative samples
-                negative_samples_indices = self.generate_neg_samples(focus_word_index, postive_samples_indices)
-
-                negative_samples = [self.i2w[index] for index in negative_samples_indices]
-
-                self.gradient_descent(
-                    focus_word_index,
-                    postive_samples,
-                    negative_samples,
-                    i,
-                    N,
-                    ep,
-                )
-
-    def generate_neg_samples(self, focus_word_idx, pos_sample_dix):
-        neg_samples = []
-        for pos_sample_ind in pos_sample_dix:
-            neg_samples.extend(self.negative_sampling(self.__nsample, focus_word_idx, pos_sample_ind))
-        return neg_samples
-
-    def gradient_descent(self, focus_word_idx, pos_sample_dix, neg_sample_dix, current_words, total_words, total_epochs):
-
-        # Compute gradient with respect to the focus word
-        focus_word_gradient = np.zeros(self.__H)
-        focus_vec = self.__W[focus_word_idx]
-
-        for postive_sample in pos_sample_dix:
-            positive_vec = self.__U[self.w2i[postive_sample]]
-            focus_word_gradient += self.sigmoid(np.dot(focus_vec, positive_vec)) * positive_vec
-
-        for negative_sample in neg_sample_dix:
-            negative_vec = self.__U[self.w2i[negative_sample]]
-            focus_word_gradient += self.sigmoid(np.dot(focus_vec, negative_vec)) * negative_vec
-
-        # Compute gradient with respect to the positive samples
-        positive_grads_dic = {}
-        focus_vec = self.__W[focus_word_idx]
-        for positive_sample in pos_sample_dix:
-            positive_vec = self.__U[self.w2i[positive_sample]]
-            positive_grads_dic[self.w2i[positive_sample]] = focus_vec * (self.sigmoid(np.dot(positive_vec, focus_vec))-1)
-
-        # Compute gradient with respect to the negative samples
-        negative_grads_dic = {}
-        focus_vec = self.__W[focus_word_idx]
-        for negative_sample in neg_sample_dix:
-            negative_vec = self.__U[self.w2i[negative_sample]]
-            negative_grads_dic[self.w2i[negative_sample]] = self.sigmoid(np.dot(focus_vec, negative_vec)) * focus_vec
-
-        # Update the focus word vector
-        self.__W[focus_word_idx] -= self.__lr * focus_word_gradient
-
-        # Update the positive and negative samples vectors
-        for positive_sample in pos_sample_dix:
-            self.__U[self.w2i[positive_sample]] -= self.__lr * positive_grads_dic[self.w2i[positive_sample]]
-
-        for negative_sample in neg_sample_dix:
-            self.__U[self.w2i[negative_sample]] -= self.__lr * negative_grads_dic[self.w2i[negative_sample]]
-
-        # Update the learning rate
-        if self.__lr < self.__init_lr * 0.0001:
-            self.__lr = self.__init_lr * 0.0001
-        else:
-            self.__lr = self.__init_lr * (1 - current_words / (total_words + 1 * total_epochs))
 
     def find_nearest(self, words, metric):
         """
@@ -355,7 +180,7 @@ class Word2Vec(object):
         (all words and distances are just example values):
     
         [[('Harry', 0.0), ('Hagrid', 0.07), ('Snape', 0.08), ('Dumbledore', 0.08), ('Hermione', 0.09)],
-        [('Potter', 0.0), ('quickly', 0.21), ('asked', 0.22), ('lied', 0.23), ('okay', 0.24)]]
+         [('Potter', 0.0), ('quickly', 0.21), ('asked', 0.22), ('lied', 0.23), ('okay', 0.24)]]
         
         The i-th element of the LLT would correspond to k nearest neighbors for the i-th word in the `words`
         list, provided as an argument. Each tuple contains a word and a similarity/distance metric.
@@ -371,6 +196,7 @@ class Word2Vec(object):
         #
         return []
 
+
     def write_to_file(self):
         """
         Write the model to a file `w2v.txt`
@@ -379,12 +205,11 @@ class Word2Vec(object):
             with open("w2v.txt", 'w') as f:
                 W = self.__W
                 f.write("{} {}\n".format(self.__V, self.__H))
-                i=0
                 for i, w in enumerate(self.__i2w):
                     f.write(w + " " + " ".join(map(lambda x: "{0:.6f}".format(x), W[i,:])) + "\n")
-                    i+=1
         except:
             print("Error: failing to write model to the file")
+
 
     @classmethod
     def load(cls, fname):
@@ -410,6 +235,7 @@ class Word2Vec(object):
             print("Error: failing to load the model to the file")
         return w2v
 
+
     def interact(self):
         """
         Interactive mode allowing a user to enter a number of space-separated words and
@@ -425,6 +251,7 @@ class Word2Vec(object):
                 print("Neighbors for {}: {}".format(w, n))
             text = input('> ')
 
+
     def train_and_persist(self):
         """
         Main function call to train word embeddings and being able to input
@@ -433,7 +260,7 @@ class Word2Vec(object):
         self.train()
         self.write_to_file()
         self.interact()
-
+        
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='word2vec embeddings toolkit')
